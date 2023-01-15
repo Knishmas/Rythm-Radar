a57a2ff8fb5ec03356caec045719619052a710b5
Andres <AndresEM1524@gmail.com>
2023-01-15 10:27:25 -0800

    Setting up authorization, currently exchanging authorization code for access token

---
+++ index.js
@@ -0,0 +1,92 @@
+require('dotenv').config()
+
+const express = require('express'); 
+const app = express(); 
+const port = 8888; 
+
+
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI= process.env.REDIRECT_URI
+const axios = require('axios');
+
+
+
+app.get('/',(req,res)=> {
+    const data = {
+        name: 'Andres', 
+        isAwesome: true
+    };
+    res.json(data);
+});
+
+ 
+    const generateRandomString = length => {
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+      };
+      
+    const stateKey = 'spotify_auth_state';
+
+
+app.get('/login', (req,res) =>{
+    const state = generateRandomString(16);
+    res.cookie(stateKey,state);
+
+    const scope = 'user-read-private user-read-email';
+
+    const searchparams = new URLSearchParams({
+
+          client_id: CLIENT_ID,
+        
+          response_type: 'code',
+        
+          redirect_uri: REDIRECT_URI,
+
+            state: state,
+
+            scope: scope
+        })
+
+    
+    res.redirect(`https://accounts.spotify.com/authorize?${searchparams}`);
+})
+
+app.get('/callback', (req,res) =>{
+    const code = req.query.code || null;
+    console.log('req query', req.query)
+    console.log('redirect URI: ', REDIRECT_URI);
+   
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: new URLSearchParams({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+          }),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+      })
+        .then(response => {
+            console.log('response: ', response);
+            if (response.status === 200) {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+            } else {
+            res.send(response);
+            }
+        })
+        .catch(error => {
+            res.send(error);
+        });
+});
+
+app.listen(port, ()=>{
+    console.log(`Express app is listening at http://localhost:${port}` );
+})
