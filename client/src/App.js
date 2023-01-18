47122c8ffe034a59625159556b1ca8434aa59573
Andres <AndresEM1524@gmail.com>
2023-01-18 10:38:52 -0800

    seperating query param logic in it's own file: spotify.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
 import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { access_token } from './spotify';
 
 function App() {
+  const [token,setToken] = useState(null);
+
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get('access_token');
-    const refreshToken = urlParams.get('refresh_token');
-
-    console.log(accessToken);
-    console.log(refreshToken);
-
-
-    if (refreshToken) {
-      fetch(`/refresh_token?refresh_token=${refreshToken}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-    }
-
+    setToken(access_token);
   }, []);
   
 
