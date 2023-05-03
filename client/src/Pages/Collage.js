294004a85ad5e9ee35db1c0e640029c7d24e4019
Andres <AndresEM1524@gmail.com>
2023-05-02 19:46:44 -0700

    Added collage page

---
+++ client/src/Pages/Collage.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { getUserTopArtists } from '../spotify';
+import { catchErrors } from '../util';
+
+const Collage = () => {
+
+}
+
+export default Collage; 
