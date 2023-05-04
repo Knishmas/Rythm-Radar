d8477dbebe09de9005d7f51a7d24dbe5fedd5ae7
Andres <AndresEM1524@gmail.com>
2023-05-03 23:56:16 -0700

    Rough setup for image collage layed out

--- client/src/Pages/Collage.js
+++ client/src/Pages/Collage.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+// array of image URLs
+const images = [
+  'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
+  'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
+  // ... add 13 more images here
+];
+
+const imageStyles = images.map(() => {
+  const width = Math.floor(Math.random() * 300 + 100); // random width between 100 and 400 pixels
+  const height = Math.floor(Math.random() * 300 + 100); // random height between 100 and 400 pixels
+
+  return {
+    width: `${width}px`,
+    height: `${height}px`,
+  };
+});
+
 function Collage() {
   return (
     <div>
-      <h1>Collage page is working</h1>
+      <h1>collage is working</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {images.map((image, index) => (
+        <img key={index} src={image} style={imageStyles[index]} alt={`Image ${index}`} />
+      ))}
+    </div>
     </div>
   );
 }
 
 export default Collage;
+
+
