a8da70464e5a912554a31f6467e81eecb5ba0074
Andres <AndresEM1524@gmail.com>
2023-05-13 12:37:19 -0700

    Changed images to users top 15 artists, changed random image size to be consistent with square shapes, added spotify fetch info logic

--- client/src/Pages/Collage.js
+++ client/src/Pages/Collage.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import { getUserTopArtists } from '../spotify';
+import { useState, useEffect } from 'react';
+import { catchErrors } from '../util';
 
-// array of image URLs
-const images = [
-  'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-  'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-  // ... add 13 more images here
-];
+const Collage = () =>{ 
+  const [topArtists, setTopArtists] = useState(null);
 
-const imageStyles = images.map(() => {
-  const width = Math.floor(Math.random() * 300 + 100); // random width between 100 and 400 pixels
-  const height = Math.floor(Math.random() * 300 + 100); // random height between 100 and 400 pixels
+  useEffect(() => {
+    const fetchData = async () =>{
+      const userTopArtists  = await getUserTopArtists();
+      setTopArtists(userTopArtists.data);
+    };
+    catchErrors(fetchData());
+  }, [])
 
-  return {
-    width: `${width}px`,
-    height: `${height}px`,
-  };
-});
+  const images = topArtists?.items?.slice(0, 15).map((artist) => {
+    return artist.images[0].url;
+  });
 
-function Collage() {
+  const imageStyles = images?.map(() => {
+    const size = Math.floor(Math.random() * 300 + 100); // b/w 100 and 400
+
+  
+    return {
+      width: `${size}px`,
+      height: `${size}px`,
+    };
+  });
+
   return (
     <div>
       <h1>collage is working</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {images.map((image, index) => (
-        <img key={index} src={image} style={imageStyles[index]} alt={`Image ${index}`} />
-      ))}
-    </div>
+        {images && images.map((image, index) => (
+          <img key={index} src={image} style={imageStyles[index]} alt={`Image ${index}`} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Collage;
-
-
